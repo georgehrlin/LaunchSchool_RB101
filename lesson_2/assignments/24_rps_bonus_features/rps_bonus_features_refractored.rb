50ffb372a3ded4265d7ebb52d46c26001b79a6b5
George Lin <linhairan@gmail.com>
2024-03-03 15:12:56 -0500

    Make progress on refractoring lesson 2 assignment 24

--- lesson_2/assignments/24_rps_bonus_features/rps_bonus_features_refractored.rb
+++ lesson_2/assignments/24_rps_bonus_features/rps_bonus_features_refractored.rb
@@ -1,23 +1,39 @@
 require "yaml"
-YML = YAML.load_file("rps_bonus_features_refractored.yml")
-choices = YML["choices"]
-messages = YML["messages"]
+game_data = YAML.load_file("rps_bonus_features_refractored.yml")
 
-p YML["choices"].fetch_values("abbreviation")
+MESSAGES = game_data["messages"]
+OPTIONS = game_data["options"]
+VALID_MOVES = OPTIONS.keys
+ROUNDS_TO_WIN = 3
 
-def prompt(message)
-  Kernel.puts("=> #{message}")
+def valid_abbreviations
+  valid_abbreviations = []
+  OPTIONS.values.each do |move|
+    valid_abbreviations << move["abbreviation"]
+  end
+  valid_abbreviations
 end
 
-def check_abbreviation?(input)
-  abbreviation_is_present = false
-  until abbreviation_is_present
-    is_present == choices.values.each { |hash| input == hash["abbreviation"]}
-  end
+VALID_ABBREVIATIONS = valid_abbreviations
+
+def prompt(message)
+  puts("=> #{message}")
+end
+
+def valid_abbreviation?(input)
+  VALID_ABBREVIATIONS.include?(input)
+end
+
+def abbreviation_to_full_move(abbreviation)
+  VALID_MOVES[VALID_ABBREVIATIONS.index(abbreviation)]
+end
+
+def get_user_move(move)
+  move = gets.chomp
 end
 
 def win?(first, second)
-  WIN_CONDITIONS[first].include?(second)
+  MOVES[first]["beats"].include?(second)
 end
 
 def display_results(player, computer)
@@ -34,21 +50,21 @@ def display_scores(player_score, computer_score)
   prompt("Your score: #{player_score}; computer score: #{computer_score}.")
 end
 
+prompt(MESSAGES["welcome"])
+prompt(MESSAGES["rules"])
+prompt(MESSAGES["instructions"])
+
 loop do # Main loop
-  player_score = 0
-  computer_score = 0
+  current_score = { player_score: 0, computer_score: 0 }
 
-  while player_score < ROUNDS_TO_WIN && computer_score < ROUNDS_TO_WIN
-    choice = ''
+  while current_score[:player_score] < ROUNDS_TO_WIN &&
+        current_score[:computer_score] < ROUNDS_TO_WIN
+    move = ''
     loop do
-      prompt("Choose one: #{VALID_CHOICES.join(', ')}.")
-      prompt("Acronyms are allowed: r, p, sc, sp, l.")
-      choice = Kernel.gets().chomp()
+      prompt("Choose one: #{MOVES.keys.join(', ')}.")
+      move = get_user_move
 
-      # Bonus Features #2
       if CHOICES_ACRONYMS.include?(choice)
-        # Part of my first attempt
-        # choice = acronym_to_full_word(choice)
         choice = CHOICES_AND_ACRONYMS[choice]
       end
 
